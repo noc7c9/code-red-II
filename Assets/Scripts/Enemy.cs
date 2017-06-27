105d63186a7b9ca336bb92f569c1175eed43db7f
Athir Saleem <noc7c9@gmail.com>
2017-06-27 22:58:03 +1000

    Complete ep6: Basic Enemy Attacking

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,26 +8,98 @@ using UnityEngine.AI;
 [RequireComponent (typeof (NavMeshAgent))]
 public class Enemy : LivingEntity {
 
+    enum State {
+        Chasing, Attacking,
+    }
+    State currentState;
+
+    public float attackDistanceThreshold;
+    public float timeBetweenAttacks;
+    public float attackSpeed;
+    public Color attackingColor;
+
     float pathRefreshRate = 0.25f;
 
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
 
     protected override void Start() {
         base.Start();
 
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
 
         StartCoroutine(UpdatePath());
     }
 
-    IEnumerator UpdatePath() {
-        while (target != null && !dead) {
-            Vector3 targetPosition = new Vector3(
-                    target.position.x, 0, target.position.z);
-            pathfinder.SetDestination(targetPosition);
+    void Update() {
+        if (Time.time > nextAttackTime) {
+            float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+            if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2)) {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
 
+    IEnumerator Attack() {
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - dirToTarget * myCollisionRadius;
+
+        float percent = 0;
+
+        // stop chasing
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        // change color when attacking
+        skinMaterial.color = attackingColor;
+
+        // lunge animation
+        while (percent <= 1) {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(
+                    originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        // restore original color
+        skinMaterial.color = originalColor;
+
+        // start chasing again
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
+    }
+
+    IEnumerator UpdatePath() {
+        // while alive and there is a target, move towards the target
+        while (target != null && !dead) {
+            if (currentState == State.Chasing) {
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position
+                    - dirToTarget * (myCollisionRadius + targetCollisionRadius
+                            + attackDistanceThreshold / 2);
+                pathfinder.SetDestination(targetPosition);
+            }
+
+            // recalculate path at an interval for performance
             yield return new WaitForSeconds(pathRefreshRate);
         }
     }
