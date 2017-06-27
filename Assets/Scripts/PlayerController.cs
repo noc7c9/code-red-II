b811130d1599cd00f91a066dbdd5faa8d5e43266
Athir Saleem <noc7c9@gmail.com>
2017-06-27 19:43:32 +1000

    Complete ep1: Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Defines the player entity's behaviour.
+ */
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        myRigidbody.MovePosition(myRigidbody.position
+                + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 direction) {
+        velocity = moveSpeed * Vector3.ClampMagnitude(direction, 1f);
+    }
+
+    public void LookAt(Vector3 point) {
+        // height correct the point, so that player doesn't look down
+        point = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(point);
+    }
+
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Handles grabbing input for the player character
+ * and passes it off to the player controller component.
+ */
+[RequireComponent (typeof (PlayerController))]
+public class PlayerInput : MonoBehaviour {
+
+    Camera viewCamera;
+    PlayerController controller;
+
+    void Start() {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update() {
+        controller.Move(GetMoveInput());
+
+        controller.LookAt(GetLookAtPoint());
+    }
+
+    Vector3 GetMoveInput() {
+        // get input axis
+        Vector3 moveInput = new Vector3(
+                Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        return moveInput.normalized;
+    }
+
+    Vector3 GetLookAtPoint() {
+        // raycast to figure out where on the ground the mouse is pointing at.
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float intersectDistance;
+
+        if (groundPlane.Raycast(ray, out intersectDistance)) {
+            Vector3 point = ray.GetPoint(intersectDistance);
+            return point;
+        }
+        return Vector3.zero;
+    }
+
+}
