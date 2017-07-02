757741ae6a1fe6982c50aedd0c37e3d082da12e6
Athir Saleem <noc7c9@gmail.com>
2017-07-02 20:22:32 +1000

    Make camera follow player

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Behaviour of the main camera.
+ */
+public class MainCamera : MonoBehaviour {
+
+    public GameObject followTarget;
+    public float smoothTime;
+    public float maxSpeed;
+
+    Vector3 initialDiff;
+    Vector3 smoothVel;
+
+    void Start() {
+        if (followTarget != null) {
+            initialDiff = transform.position - followTarget.transform.position;
+        }
+    }
+
+    void Update() {
+        if (followTarget != null) {
+            Vector3 targetPos = followTarget.transform.position + initialDiff;
+            transform.position = Vector3.SmoothDamp(transform.position,
+                    targetPos, ref smoothVel, smoothTime, maxSpeed);
+        }
+    }
+
+}
