294ca13ece1f1491f918db8c307d7d08818d72db
Athir Saleem <noc7c9@gmail.com>
2017-09-07 14:56:48 +1000

    Create person model

---
+++ Assets/Game/Scripts/AnimateRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Noc7c9.TheDigitalFrontier {
+
+    public class AnimateRotation : MonoBehaviour {
+
+        public Vector3 axis;
+        public float speed;
+
+        void Update() {
+            transform.Rotate(axis, speed * Time.deltaTime);
+        }
+
+    }
+
+}
