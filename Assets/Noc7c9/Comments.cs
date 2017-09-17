ae991a8df19428732a3aa40c1e8523e6400f153e
Athir Saleem <noc7c9@gmail.com>
2017-09-17 14:58:20 +1000

    Create a comments component

---
+++ Assets/Noc7c9/Comments.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Noc7c9 {
+
+    public class Comments : MonoBehaviour {
+
+        public string comments = "No comment.";
+        public bool inEditMode;
+
+        public void ToggleEditMode() {
+            inEditMode = !inEditMode;
+        }
+
+    }
+
+}
---
+++ Assets/Noc7c9/Editor/CommentsEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Noc7c9 {
+
+    [CustomEditor (typeof (Comments))]
+    public class CommentsEditor : Editor {
+
+        public override void OnInspectorGUI() {
+            Comments comments = (Comments) target;
+
+            comments.inEditMode = GUILayout.Toggle(
+                    comments.inEditMode, "Toggle Edit Mode");
+
+            if (comments.inEditMode) {
+                comments.comments = GUILayout.TextArea(comments.comments);
+            } else {
+                GUILayout.Label(comments.comments);
+            }
+        }
+
+    }
+
+}
